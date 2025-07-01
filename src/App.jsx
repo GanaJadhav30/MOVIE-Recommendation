f2b8a1093450f14422ecbabc043da92495171fff
GanaJadhav30 <jadhavganeshatclg@gmail.com>
2025-07-01 22:29:17 +0530

    add header section

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,35 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Search from './components/Search'
 
-const Cards = ({title}) =>{
-  return <h2>{title}</h2>
-}
+// const Cards = ({title}) =>{
+//   const [hasLiked, sethasLiked] = useState(false)
+//   return <div>
+//     <h2>{title}</h2>
+//     <button onClick={()=>{sethasLiked(!hasLiked)}}>{hasLiked ? 'Liked' : 'like'}</button>
+//   </div>
+// }
 
 const App = () =>{
-  return <div>
-     <h2>Arrow Function</h2>
-      <Cards  title='spiderman'/>
-      <Cards title='batman' />
-      <Cards title='superman' />
-  </div>
+
+   const [searchTerm, setsearchTerm] = useState('')
+
+  return <main>
+     <div className='pattern'> 
+       <div className='wrapper'>
+          <header>
+            <img src="./hero.png" alt="Hero banner" />
+           <h1><span className='text-gradient'>Find Movies</span> You'll Enjoy  Without Any Hassel</h1>
+           </header>
+           <Search searchTerm={searchTerm} setsearchTerm={setsearchTerm} />
+           <h1 className='text-white text-2xl'>{searchTerm}</h1>
+          
+        </div>
+     </div>
+
+  </main> 
+  
 }
 
 export default App
