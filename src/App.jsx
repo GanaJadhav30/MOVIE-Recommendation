4b52305a4a497e54b25433a420e3b7039c4ba707
GanaJadhav30 <jadhavganeshatclg@gmail.com>
2025-07-04 23:55:04 +0530

    implement search

--- src/App.jsx
+++ src/App.jsx
@@ -28,13 +28,13 @@ const App = () =>{
    const [movieList, setmovieList] = useState([])
    const [isLoading, setisLoading] = useState(false)
 
-   const fetchMovies = async () =>{
+   const fetchMovies = async (query='') =>{
 
       setisLoading(true)
       setfetchingError('')
       try {
 
-         const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+         const endpoint = query ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`   : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
          const response = await fetch(endpoint,API_OPTIONS)
 
          if(!response.ok){
@@ -60,8 +60,8 @@ const App = () =>{
       }
    }
    useEffect(() => {
-      fetchMovies()
-   }, [])
+      fetchMovies(searchTerm)
+   }, [searchTerm])
    
 
   return <main>
